faffc59e6c3be58f5c03e5c35af2588b201d3361
ARRECODE <axelromero99@outlook.com>
2023-04-29 20:02:52 -0300

    feat: libraries for phrases and basic functionality

--- src/pages/auxPhrases.jsx
+++ src/pages/auxPhrases.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import Phrases from "../views/Phrases";
+import { Box } from "@chakra-ui/react";
 
 const auxPhrases = () => {
   return (
     <>
+      {/* make two big boxes with 100vh. CODE*/}
+      <Box bg="blue" w="100" h="100vh">
+        asdads
+      </Box>
       <Phrases />;
     </>
   );
--- src/views/Phrases.jsx
+++ src/views/Phrases.jsx
@@ -1,16 +1,71 @@
 import { Box, Center, Text } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 
 const Phrases = () => {
+  const [isVisible, setIsVisible] = useState([false, false, false]);
+
+  const handleIntersection = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        setIsVisible([true, true, true]);
+      }
+    });
+  };
+
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+    triggerOnce: true,
+    rootMargin: "0px",
+    initialInView: false,
+    skip: false,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      setTimeout(() => setIsVisible([true, false, false]), 0);
+      setTimeout(() => setIsVisible([true, true, false]), 1000);
+      setTimeout(() => setIsVisible([true, true, true]), 2000);
+    }
+  }, [inView]);
+
   return (
     <Center h="100vh">
       <Box textAlign="center">
-        <Text fontSize="6xl" fontWeight="bold" mb="8">
+        <Text
+          fontSize="8xl"
+          fontWeight="bold"
+          mb="8"
+          ref={ref}
+          style={{
+            opacity: isVisible[0] ? 1 : 0,
+            transition: "opacity 0.5s ease-in-out",
+          }}
+        >
           EMPATHIZE
         </Text>
-        <Text fontSize="6xl" fontWeight="bold" mb="8">
+        <Text
+          fontSize="8xl"
+          fontWeight="bold"
+          mb="8"
+          ref={ref}
+          style={{
+            opacity: isVisible[1] ? 1 : 0,
+            transition: "opacity 0.5s ease-in-out",
+          }}
+        >
           IDEATE
         </Text>
-        <Text fontSize="6xl" fontWeight="bold" mb="8">
+        <Text
+          fontSize="8xl"
+          fontWeight="bold"
+          mb="8"
+          ref={ref}
+          style={{
+            opacity: isVisible[2] ? 1 : 0,
+            transition: "opacity 0.5s ease-in-out",
+          }}
+        >
           EXE<span style={{ color: "pink" }}>CUTE</span>
         </Text>
       </Box>
