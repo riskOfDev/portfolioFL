89968bf1dfd383603c7061b5138cbe2100f53994
ARRECODE <axelromero99@outlook.com>
2023-04-29 20:10:59 -0300

    feat: basics for projects and readmos

---
+++ src/views/Projects.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Project from "../components/Project";
+
+const Projects = () => {
+  // list of projects ( refactor to integration with readmos )
+  const projects = [
+    {
+      name: "Project 1",
+      description: "This is a project",
+      link: "https://www.google.com",
+    },
+    {
+      name: "Project 2",
+      description: "This is a project",
+      link: "https://www.google.com",
+    },
+    {
+      name: "Project 3",
+      description: "This is a project",
+      link: "https://www.google.com",
+    },
+  ];
+
+  return (
+    <>
+      <div>Projects</div>
+      {projects.map((project, index) => (
+        <Project
+          key={index}
+          name={project.name}
+          description={project.description}
+          link={project.link}
+        />
+      ))}
+    </>
+  );
+};
+
+export default Projects;
