9e24c67aefa1103aeab75a14582ac0b71a15ba58
arrecode <axelromero99@outlook.com>
2023-06-10 17:42:45 -0300

    .

--- src/pages/auxHero.jsx
+++ src/pages/auxHero.jsx
@@ -5,7 +5,6 @@ import { Box } from "@chakra-ui/react";
 const auxHero = () => {
   return (
     <>
-      <Box bg="blue" w="100" h="100vh"></Box>
       <Hero />
       <Box bg="blue" w="100" h="100vh"></Box>
     </>
--- src/views/Hero.jsx
+++ src/views/Hero.jsx
@@ -1,28 +1,50 @@
 import React from "react";
 import styles from "../styles/Hero.module.css";
+import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div>
-      {/* Middle Tittle and Subtitle */}
-      <div>
+    <div className={styles.container}>
+      <div className={styles.title}>
         <h1>HELLO, I'M FLORENCIA</h1>
         <h2>UX/UI DESIGNER</h2>
       </div>
-      {/* Motto */}
+
       <div>
-        <div>
-          I <span className="design">DESIGN</span>
-        </div>
-        <div>WHAT YOU</div>
-        <div>
-          <span className="love">LOVE</span> TO USE
+        <div className={styles.line}>
+          <img alt="line" src="/line/ESCALERA.png" />
         </div>
       </div>
-      {/* Line */}
-      <div></div>
-      {/* Social Media */}
-      <div></div>
+
+      <div className={styles.socialMedia}>
+        <ul>
+          <li>
+            <a>
+              <Image alt="mail" width="50" height="50" src="/social/mail.png" />
+            </a>
+          </li>
+          <li>
+            <a>
+              <Image
+                alt="behance"
+                width="50"
+                height="50"
+                src="/social/behance.png"
+              />
+            </a>
+          </li>
+          <li>
+            <a>
+              <Image
+                alt="linkedin"
+                width="50"
+                height="50"
+                src="/social/linkedin.png"
+              />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
