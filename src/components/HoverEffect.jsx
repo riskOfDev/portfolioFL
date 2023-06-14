22b63b86e3c640e89ad71acab05560579096f348
arrecode <axelromero99@outlook.com>
2023-06-14 15:35:44 -0300

    funciona el hover

--- src/components/HoverEffect.jsx
+++ src/components/HoverEffect.jsx
@@ -3,19 +3,30 @@ import styles from "../styles/HoverEffect.module.css";
 
 const HoverEffect = () => {
   const containerRef = useRef(null);
+  const cursorPosRef = useRef({ x: 0, y: 0 });
 
   useEffect(() => {
     const onMouseMove = (event) => {
-      const x = event.clientX;
-      const y = event.clientY;
+      cursorPosRef.current = { x: event.clientX, y: event.clientY };
+      const x = event.clientX + window.pageXOffset;
+      const y = event.clientY + window.pageYOffset;
       containerRef.current.style.setProperty("--x", `${x}px`);
       containerRef.current.style.setProperty("--y", `${y}px`);
     };
 
-    containerRef.current.addEventListener("mousemove", onMouseMove);
+    const onScroll = () => {
+      const x = cursorPosRef.current.x + window.pageXOffset;
+      const y = cursorPosRef.current.y + window.pageYOffset;
+      containerRef.current.style.setProperty("--x", `${x}px`);
+      containerRef.current.style.setProperty("--y", `${y}px`);
+    };
+
+    window.addEventListener("mousemove", onMouseMove);
+    window.addEventListener("scroll", onScroll);
 
     return () => {
-      containerRef.current.removeEventListener("mousemove", onMouseMove);
+      window.removeEventListener("mousemove", onMouseMove);
+      window.removeEventListener("scroll", onScroll);
     };
   }, []);
 
